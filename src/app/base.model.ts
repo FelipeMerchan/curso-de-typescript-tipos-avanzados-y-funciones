2af45e7ec278820f091416325f84f1cf55aa940d
Felipe Merchan <felipemerchanfuquen@gmail.com>
2025-04-04 09:31:06 -0500

    Clase 15 de 22 - Propiedades de solo lectura

--- src/app/base.model.ts
+++ src/app/base.model.ts
@@ -1,5 +1,7 @@
 export interface BaseModel {
-  id: string;
-  createdAt: Date;
+  /* readonly nos permite evitar la sobre escritura a ciertas variables,
+  es decir, protegerlas, con esto solo serán de lectura. */
+  readonly id: string;
+  readonly createdAt: Date;
   updatedAt: Date;
 }
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -3,5 +3,10 @@ import { Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  /* Como marcamos la propiedad id como readonly
+  no vamos a poder sobreescribirla, solo la podrémos leer
+  (Cannot assign to 'id' because it is a read-only property): */
+  // data.id = 'ashjdhjasdhj' // estamos sobreescribiendo el id
+  // data.createdAt = new Date(1998, 1, 1);
   products.push(data);
 }
